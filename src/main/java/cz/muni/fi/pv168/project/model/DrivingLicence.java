8083748b64e92f1acc65e8f72c29245c5f301003
Marek Zanechal <xzanech@fi.muni.cz>
2023-10-14 16:46:58 +0200

    Added dialog and dirivng licence classes

---
+++ src/main/java/cz/muni/fi/pv168/project/model/DrivingLicence.java
@@ -0,0 +1,35 @@
+package cz.muni.fi.pv168.project.model;
+
+import java.time.LocalDateTime;
+
+public class DrivingLicence {
+
+    private LocalDateTime to;
+    private boolean valid;
+
+    public DrivingLicence(LocalDateTime to) {
+        setTo(to);
+        valid = checkLicence();
+    }
+
+    public LocalDateTime getTo() {
+        return to;
+    }
+
+    public void setTo(LocalDateTime to) {
+        this.to = to;
+    }
+
+    public void setValid(boolean valid) {
+        this.valid = valid;
+    }
+
+    public boolean getValid() {
+        return this.valid;
+    }
+
+    public boolean checkLicence() {
+        LocalDateTime now = LocalDateTime.now();
+        return now.isBefore(this.getTo().plusDays(1));
+    }
+}
